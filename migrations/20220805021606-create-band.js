97679ea4e02a4f394fa69346a8b6ed2bb024f8f9
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-08-07 12:43:12 -0500

    cleaning up

--- migrations/20220805021606-create-band.js
+++ migrations/20220805021606-create-band.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Bands', {
+    await queryInterface.createTable('bands', {
       band_id: {
         allowNull: false,
         autoIncrement: true,
@@ -23,6 +23,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bands');
+    await queryInterface.dropTable('bands');
   }
 };
--- server.js
+++ server.js
@@ -8,6 +8,10 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// CONTROLLERS 
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
 // SEQUELIZE CONNECTION
 //const sequelize = new Sequelize(process.env.PG_URI)
 
